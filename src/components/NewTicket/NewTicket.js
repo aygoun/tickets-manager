d7cfb437fa052c1120ac7bd5cf66f5774c098e9d
Armand BLIN <armand.blin@festival-aix.com>
2022-07-02 16:10:59 +0200

    Updating

--- src/components/NewTicket/NewTicket.js
+++ src/components/NewTicket/NewTicket.js
@@ -10,11 +10,29 @@ import {
   TextField,
   Button,
 } from "@mui/material";
+import { db } from "../../firebase";
 
 function NewTicket() {
   let navigate = useNavigate();
 
   const [tag, setTag] = useState("");
+  const [resume, setResume] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    db.collection("tickets")
+      .add({
+        tag: tag,
+        resume: resume,
+        description: description,
+        createdAt: new Date(),
+      })
+      .then(() => {
+        navigate("/");
+      }
+      );
+  };
 
   return (
     <div>
@@ -27,16 +45,20 @@ function NewTicket() {
           <div className="newticket-ticket-header-container flex1">
             <div className="newticket-tag-input-container">
               <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Tag</InputLabel>
+                <InputLabel id="demo-simple-select-label">Sujet</InputLabel>
                 <Select
                   labelId="demo-simple-select-label"
                   id="demo-simple-select"
                   value={tag}
-                  label="Tag"
+                  label="Sujet"
                   onChange={(e) => setTag(e.target.value)}
                 >
-                  <MenuItem value={"Teams"}>Teams</MenuItem>
-                  <MenuItem value={"SecuTix"}>SecuTix</MenuItem>
+                  <MenuItem value={"Logiciels"}>Logiciels</MenuItem>
+                  <MenuItem value={"Matériels"}>Matériels</MenuItem>
+                  <MenuItem value={"Prêt"}>Prêt</MenuItem>
+                  <MenuItem value={"Comptes"}>Comptes</MenuItem>
+                  <MenuItem value={"Réseaux"}>Réseaux</MenuItem>
+                  <MenuItem value={"Déménagement"}>Déménagement</MenuItem>
                   <MenuItem value={"Autre"}>Autre</MenuItem>
                 </Select>
               </FormControl>
@@ -44,9 +66,11 @@ function NewTicket() {
             <div className="newticket-subject-input-container">
               <TextField
                 id="filled-basic"
-                label="Sujet"
+                label="Résumé"
                 variant="filled"
                 className="newticket-subject-input"
+                value={resume}
+                onChange={(e) => setResume(e.target.value)}
               />
             </div>
           </div>
@@ -54,13 +78,15 @@ function NewTicket() {
           <div className="newticket-body-input-container flex1">
             <textarea
               type="text"
-              placeholder="Remarques"
+              placeholder="Détails"
               className="newticket-body-input"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
           </div>
           <div className="flex1 newticket-validate-button-container">
             <div className="flex1">
-              <Button variant="contained">Envoyer</Button>
+              <Button variant="contained" onClick={handleSubmit}>Envoyer</Button>
             </div>
             <div className="newticket-cancel-button-flex-none">
               <Button variant="outlined" onClick={() => navigate("/dashboard")}>
--- src/components/items/TicketDetail.js
+++ src/components/items/TicketDetail.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import "./styles.css";
+import arrow from "../../assets/arrow.png";
 
 
 function TicketDetail() {
   return (
-    <div>
-        
-        <div>TicketDetail</div>
+    <div className="ticket-preview-container">
+      <div className="ticket-preview-subcontainer">
+        <div className="ticket-preview-tag-container flex1">
+          Logiciels
+        </div>
+        <div className="ticket-preview-subject-container flex1">
+          SecuTix problème de connexion
+        </div>
+        <div className="ticket-preview-info-container flex1">
+          <a href=""><img src={arrow} className="ticket-preview-info-img" width="35" /></a>
+        </div>
+      </div>
     </div>
   );
 };
