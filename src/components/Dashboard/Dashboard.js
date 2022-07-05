510df499437b080af16d6932dbf96cefa2fa29f9
Armand B <armand.blin@gmail.com>
2022-07-05 14:16:28 +0200

    Updating

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,19 @@
 import "./Dashboard.css";
 import React, { useEffect, useState } from "react";
 import Header from "../items/Header";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
 import plus from "../../assets/plus.png";
 import TicketDetail from "../items/TicketDetail";
 import { useNavigate } from "react-router-dom";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+
 
 function Dashboard() {
   let navigate = useNavigate();
@@ -12,14 +21,55 @@ function Dashboard() {
   const userEmail = sessionStorage.getItem("userEmail");
   const [userTickets, setUserTickets] = useState([]);
   const [noTickets, setNoTickets] = useState("");
+  const [openNb, setOpenNb] = useState([]);
+  const [closedNb, setClosedNb] = useState([]);
+  const [affectedNb, setAffectedNb] = useState([]);
+  const [suspendedNb, setSuspendedNb] = useState([]);
 
   const handleNewTicket = () => {
     navigate("/new-ticket");
   };
 
+  const fetchTickets = async () => {
+    const q = query(collection(db, "tickets"), where("from", "==", userEmail), orderBy("date", "desc"));
+      const querySnapshot = onSnapshot (q, (querySnapshot) => {
+        const tickets = querySnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        if (tickets.length === 0) {
+          setNoTickets("Aucun ticket");
+        }else{
+          setNoTickets("");
+          setUserTickets(tickets);
+          tickets.forEach(element => {
+            switch (element.status) {
+              case "open":
+                setOpenNb(openNb => [...openNb, element]);
+                break;
+              case "closed":
+                setClosedNb(closedNb => [...closedNb, element]);
+                break;
+              case "affected":
+                setAffectedNb(affectedNb => [...affectedNb, element]);
+                break;
+              case "suspended":
+                setSuspendedNb(suspendedNb => [...suspendedNb, element]);
+                break;
+              default:
+                break;
+            }
+          });
+        }
+      }
+      );
+  };
+
   useEffect(() => {
-    if (userTickets.length === 0) {
-      setNoTickets("Aucun ticket");
+    if (Object.entries(userTickets).length === 0 && noTickets === "") {
+      fetchTickets();
     }
   }, [userTickets]);
 
@@ -39,16 +89,31 @@ function Dashboard() {
           </a>
         </div>
         <div className="dashboard-content-body">
-          <div className="dashboard-content-title">Anciens tickets :</div>
-
-          <div>
-          <TicketDetail />
-            {userTickets &&
-              userTickets.map((ticket, index) => (
-                <TicketDetail key={index} ticket={ticket} />
-              ))}
-            {<h3 align="center">{noTickets}</h3>}
-            <hr />
+          <div className="dashboard-content-title">Mes tickets :</div>
+          <div className="dashboard-content-body-subcontainer">
+            <div className=" dashboard-content-body-filtermenu">
+              <a href="#" className="dashboard-content-body-menu">
+                <div className="dashboard-content-filter-choices">OUVERT {openNb.length/2}</div>
+              </a>
+              <a href="#" className="dashboard-content-body-menu">
+                <div className="dashboard-content-filter-choices">
+                  AFFECTÉ {affectedNb.length/2}
+                </div>
+              </a>
+              <a href="#" className="dashboard-content-body-menu">
+                <div className="dashboard-content-filter-choices">SUSPENDU {suspendedNb.length/2}</div>
+              </a>
+              <a href="#" className="dashboard-content-body-menu">
+                <div className="dashboard-content-filter-choices" style={{borderBottom: '0px solid'}}>FERMÉ {closedNb.length/2}</div>
+              </a>
+            </div>
+            <div className=" dashboard-content-body-content-container">
+              {userTickets &&
+                userTickets.map((ticket, key) => {
+                  return <TicketDetail ticket={ticket} key={key} />;
+                })}
+              {<h3 align="center">{noTickets}</h3>}
+            </div>
           </div>
         </div>
       </div>
--- src/components/items/TicketDetail.js
+++ src/components/items/TicketDetail.js
@@ -1,20 +1,40 @@
 import React, { useState } from "react";
 import "./styles.css";
 import arrow from "../../assets/arrow.png";
+import remove from "../../assets/remove.png";
+import { db } from "../../firebase";
+import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 
-function TicketDetail() {
+
+function TicketDetail(props) {
+  let navigate = useNavigate();
+  const ticket = props.ticket;
+
+  const handleDelete = () => {
+    console.log("HELLO: " + ticket.ticketID);
+    const docRef = doc(db, "tickets", "" + ticket.ticketID);
+    updateDoc(docRef, { status: "closed" });
+  };
+
   return (
     <div className="ticket-preview-container">
       <div className="ticket-preview-subcontainer">
-        <div className="ticket-preview-tag-container flex1">
-          Logiciels
-        </div>
+        <div className="ticket-preview-tag-container flex1">{ticket.tag}</div>
         <div className="ticket-preview-subject-container flex1">
-          SecuTix problème de connexion
+          {ticket.object}
         </div>
         <div className="ticket-preview-info-container flex1">
-          <a href=""><img src={arrow} className="ticket-preview-info-img" width="35" /></a>
+          <div className="ticket-preview-info-status-container">
+            {ticket.status.toUpperCase()}
+          </div>
+          <span className="flex1 span-button" onClick={() => handleDelete()}>
+            <img src={remove} className="ticket-preview-close-img" width="35" />
+          </span>
+          <span className="flex1 span-button" onClick={() => navigate("/view-ticket:"+ ticket.ticketID)}>
+            <img src={arrow} className="ticket-preview-info-img" width="35" />
+          </span>
         </div>
       </div>
     </div>
