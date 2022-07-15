872793cfb74751735a32b60ff48ca54aab3c0a6f
tintinlu <brice.lansard@festival-aix.com>
2022-07-15 15:14:29 +0200

    Fixing warning

--- src/components/items/TicketDetail.js
+++ src/components/items/TicketDetail.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
 import arrow from "../../assets/arrow.png";
 import remove from "../../assets/remove.png";
 import { db } from "../../firebase";
-import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+import { doc, updateDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 
 
@@ -16,8 +16,7 @@ function TicketDetail(props) {
   let dateTmp = new Date(ticket.date.seconds * 1000);
   let dateString = dateTmp.toLocaleDateString();
   let timeString = dateTmp.toLocaleTimeString();
-  const [date, setDate] = useState(dateString + " " + timeString);
-
+  const date = dateString + " " + timeString;
   const handleDelete = () => {
     console.log("HELLO: " + ticket.ticketID);
     const docRef = doc(db, "tickets", "" + ticket.ticketID);
@@ -41,13 +40,13 @@ function TicketDetail(props) {
             {ticket.status.toUpperCase()}
           </div>
           <span className="flex1 span-button" onClick={() => handleDelete()}>
-            <img src={remove} className="ticket-preview-close-img" width="35" />
+            <img src={remove} alt="close" className="ticket-preview-close-img" width="35" />
           </span>
           <span
             className="flex1 span-button"
             onClick={() => {isAdmin ? navigate("/view-ticket:" + ticket.ticketID + ":Y") : navigate("/view-ticket:" + ticket.ticketID + ":N")}}
           >
-            <img src={arrow} className="ticket-preview-info-img" width="35" />
+            <img src={arrow} alt="preview" className="ticket-preview-info-img" width="35" />
           </span>
         </div>
       </div>
